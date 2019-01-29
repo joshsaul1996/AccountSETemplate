081959558ad6781f916f660f8da9510f1fac0226
joshsaul1996 <joshsaul1996@gmail.com>
2019-01-29 17:13:16 +0000

    Completed Task with 87.9% Coverage

--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -8,41 +8,77 @@ import org.junit.Test;
 
 import com.qa.persistence.domain.Account;
 import com.qa.persistence.repository.AccountMapRepository;
+import com.qa.persistence.util.JSONUtil;
 
 public class AccountServiceTest {
+	AccountMapRepository repository;
+	JSONUtil json = new JSONUtil();
 
 	@Before
 	public void setup() {
+		
+		repository = new AccountMapRepository();
 	
 	}
 	
 	@Test
 	public void addAccountTest() {
+		int mapSize = repository.accounts.size(); 
+		Account account = new Account("Josh", "Saul" , 22);
+		repository.createAccount(json.getJSONForObject(account));
+		assertEquals("Hash Map size did not increase by 1", mapSize + 1, repository.accounts.size());
 		
 	}
-	
+	 
 	@Test
 	public void add2AccountTest() {
+		int mapSize = repository.accounts.size();
+		Account account1 = new Account("Josh", "Saul" , 22);
+		Account account2 = new Account("William", "Ashworth" , 21);
+		repository.createAccount(json.getJSONForObject(account1));
+		repository.createAccount(json.getJSONForObject(account2));
+		assertEquals("Hash Map size did not increase by 2", mapSize + 2, repository.accounts.size());
 		
 	}
-
+ 
 	@Test
 	public void removeAccountTest() {
-		
+		Account account = new Account("Josh", "Saul" , 22);
+		repository.createAccount(json.getJSONForObject(account));
+		int mapSize = repository.accounts.size();
+		repository.deleteAccount((long) mapSize -1);
+		assertEquals("Map size did not decrease by 1", mapSize -1, repository.accounts.size());
 	}
 	
 	@Test
 	public void remove2AccountTest() {
-		
+		Account account = new Account("Josh", "Saul" , 22);
+		repository.createAccount(json.getJSONForObject(account));
+		Account account2 = new Account("William", "Ashworth" , 21);
+		repository.createAccount(json.getJSONForObject(account2));
+		int mapSize = repository.accounts.size();
+		repository.deleteAccount((long) mapSize -1);
+		repository.deleteAccount((long) mapSize -2);
+		assertEquals("Map size did not decrease by 2", mapSize -2, repository.accounts.size());
 	}
 	
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
-		
+		Account account = new Account("Josh", "Saul" , 22);
+		repository.createAccount(json.getJSONForObject(account));
+		Account account2 = new Account("William", "Ashworth" , 21);
+		repository.createAccount(json.getJSONForObject(account2));
+		int mapSize = repository.accounts.size();
+		repository.deleteAccount((long) mapSize -1);
+		repository.deleteAccount((long) mapSize -2);
+		repository.deleteAccount((long) mapSize -3);
+		assertEquals("Map size did not decrease by 2 and doesnt error if none existent is removed", mapSize -2, repository.accounts.size());
 	}
 	
 	@Test
 	public void accountConversionToJSONTestWithEmptyMap() {
+		String util = json.getJSONForObject(repository.accounts.get((long)45));
+		assertEquals("Json string did not match", "null", util);
 	
 	}
 	
@@ -53,7 +89,7 @@ public class AccountServiceTest {
 
 	@Test
 	public void accountConversionToJSONTest() {
-		
+
 	}
 
 	@Test
