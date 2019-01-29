6b6c2422a91f7921c269b3e15924a5a8c694c884
joshsaul1996 <joshsaul1996@gmail.com>
2019-01-29 11:07:54 +0000

    Added Hello World feature and spelt Hello correctly

--- src/main/java/com/qa/persistence/domain/app.java
+++ src/main/java/com/qa/persistence/domain/app.java
@@ -3,7 +3,7 @@ package com.qa.persistence.domain;
 public class app {
 	
 	public static void helloWorld() {
-		System.out.println("Hellow World to the standard out");
+		System.out.println("Hello World to the standard out");
 	}
 
 }
