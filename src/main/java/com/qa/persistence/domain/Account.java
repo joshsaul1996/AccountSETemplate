0f8bdd3e9374c7fb692fc31af288a69b53a10bf1
joshsaul1996 <joshsaul1996@gmail.com>
2019-01-30 16:07:34 +0000

    Added id firstName lastName and accountNumber database functionality

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,11 +1,21 @@
 package com.qa.persistence.domain;
-               
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column(length =20)
 	private String firstName;
+	@Column(length =20)
 	private String lastName;
+	@Column(length =8)
 	private int accountNumber;
-	
 	
 	public Account(String firstName, String lastName, int accountNumber) {
 		this.firstName = firstName;
